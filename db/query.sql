fe5cc3b046989b09625d384a5d3841d0723e48cc
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-11-16 17:46:40 -0500

    add
    query.sql changes

--- db/query.sql
+++ db/query.sql
@@ -1,13 +1,17 @@
+--join ALL of the department table info and role table only at related "id"" column
 SELECT named
-FROM departmentLEFT JOIN role
+FROM department
+LEFT JOIN role
 ON department.id = role.department_id;
 
+--join ALL of the role table info and department table only at related "id"" column
 SELECT title, salary, department_id
 FROM role
 LEFT JOIN department
 ON role.department_id = department.id;
 
+--join ALL of the employee table info and role table only at related "id"" column
 SELECT first_name, last_name, role_id, manager_id
 FROM employee
 JOIN role
-ON employee.role_id=role.id;
+ON employee.role_id = role.id;
