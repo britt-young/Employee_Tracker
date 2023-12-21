2ce847373ea7e9866f636e55bf7242c25bbb5aba
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-12-21 15:06:16 -0500

    refactor
    query comments causing issue with SOURCE

--- db/query.sql
+++ db/query.sql
@@ -1,16 +1,16 @@
---join ALL of the department table info and role table only at related "id" column
+/* --join ALL of the department table info and role table only at related "id" column */
 SELECT named
 FROM department
 LEFT JOIN role
 ON department.id = role.department_id;
 
---join ALL of the role table info and department table only at related "id" column
+/* -- join ALL of the role table info and department table only at related "id" column */
 SELECT title, salary, department_id
 FROM role
 LEFT JOIN department
 ON role.department_id = department.id;
 
---join ALL of the employee table info and role table only at related "id" column
+/* --join ALL of the employee table info and role table only at related "id" column */
 SELECT first_name, last_name, role_id, manager_id
 FROM employee
 JOIN role
